d593f3e7a49f2edc3999147f6aff7dd6078a97f3
Liam Douglass <liamadouglass@gmail.com>
2020-05-05 16:27:13 -0400

    beginning implementation of web browser front end

---
+++ client/canvas.js
@@ -0,0 +1,97 @@
+
+const testData = JSON.parse('{"tiles": [{"row": 0, "col": 0, "resource": "DESERT", "rollNum": null, "hasRobber": true}, {"row": 0, "col": 1, "resource": "WOOD", "rollNum": 3, "hasRobber": false}, {"row": 0, "col": 2, "resource": "BRICK", "rollNum": 6, "hasRobber": false}, {"row": 1, "col": 0, "resource": "STONE", "rollNum": 6, "hasRobber": false}, {"row": 1, "col": 1, "resource": "STONE", "rollNum": 8, "hasRobber": false}, {"row": 1, "col": 2, "resource": "SHEEP", "rollNum": 11, "hasRobber": false}, {"row": 1, "col": 3, "resource": "WHEAT", "rollNum": 9, "hasRobber": false}, {"row": 2, "col": 0, "resource": "WHEAT", "rollNum": 4, "hasRobber": false}, {"row": 2, "col": 1, "resource": "WHEAT", "rollNum": 12, "hasRobber": false}, {"row": 2, "col": 2, "resource": "SHEEP", "rollNum": 5, "hasRobber": false}, {"row": 2, "col": 3, "resource": "STONE", "rollNum": 10, "hasRobber": false}, {"row": 2, "col": 4, "resource": "BRICK", "rollNum": 9, "hasRobber": false}, {"row": 3, "col": 0, "resource": "WOOD", "rollNum": 10, "hasRobber": false}, {"row": 3, "col": 1, "resource": "SHEEP", "rollNum": 3, "hasRobber": false}, {"row": 3, "col": 2, "resource": "WOOD", "rollNum": 4, "hasRobber": false}, {"row": 3, "col": 3, "resource": "WOOD", "rollNum": 2, "hasRobber": false}, {"row": 4, "col": 0, "resource": "WHEAT", "rollNum": 5, "hasRobber": false}, {"row": 4, "col": 1, "resource": "BRICK", "rollNum": 8, "hasRobber": false}, {"row": 4, "col": 2, "resource": "SHEEP", "rollNum": 11, "hasRobber": false}], "paths": [{"row": 7, "col": 3, "hasRoad": false, "owner": ""}, {"row": 4, "col": 7, "hasRoad": false, "owner": ""}, {"row": 6, "col": 9, "hasRoad": false, "owner": ""}, {"row": 1, "col": 3, "hasRoad": false, "owner": ""}, {"row": 9, "col": 1, "hasRoad": false, "owner": ""}, {"row": 4, "col": 8, "hasRoad": false, "owner": ""}, {"row": 3, "col": 0, "hasRoad": false, "owner": ""}, {"row": 6, "col": 6, "hasRoad": false, "owner": ""}, {"row": 8, "col": 0, "hasRoad": false, "owner": ""}, {"row": 2, "col": 1, "hasRoad": false, "owner": ""}, {"row": 6, "col": 2, "hasRoad": false, "owner": ""}, {"row": 5, "col": 1, "hasRoad": false, "owner": ""}, {"row": 2, "col": 5, "hasRoad": false, "owner": ""}, {"row": 0, "col": 3, "hasRoad": false, "owner": ""}, {"row": 10, "col": 3, "hasRoad": false, "owner": ""}, {"row": 2, "col": 4, "hasRoad": false, "owner": ""}, {"row": 7, "col": 2, "hasRoad": false, "owner": ""}, {"row": 4, "col": 0, "hasRoad": false, "owner": ""}, {"row": 1, "col": 2, "hasRoad": false, "owner": ""}, {"row": 8, "col": 5, "hasRoad": false, "owner": ""}, {"row": 9, "col": 0, "hasRoad": false, "owner": ""}, {"row": 6, "col": 7, "hasRoad": false, "owner": ""}, {"row": 3, "col": 3, "hasRoad": false, "owner": ""}, {"row": 4, "col": 9, "hasRoad": false, "owner": ""}, {"row": 5, "col": 5, "hasRoad": false, "owner": ""}, {"row": 8, "col": 1, "hasRoad": false, "owner": ""}, {"row": 4, "col": 4, "hasRoad": false, "owner": ""}, {"row": 10, "col": 2, "hasRoad": false, "owner": ""}, {"row": 6, "col": 3, "hasRoad": false, "owner": ""}, {"row": 5, "col": 0, "hasRoad": false, "owner": ""}, {"row": 2, "col": 2, "hasRoad": false, "owner": ""}, {"row": 3, "col": 4, "hasRoad": false, "owner": ""}, {"row": 8, "col": 6, "hasRoad": false, "owner": ""}, {"row": 10, "col": 0, "hasRoad": false, "owner": ""}, {"row": 0, "col": 4, "hasRoad": false, "owner": ""}, {"row": 4, "col": 1, "hasRoad": false, "owner": ""}, {"row": 1, "col": 1, "hasRoad": false, "owner": ""}, {"row": 6, "col": 4, "hasRoad": false, "owner": ""}, {"row": 5, "col": 4, "hasRoad": false, "owner": ""}, {"row": 2, "col": 6, "hasRoad": false, "owner": ""}, {"row": 3, "col": 2, "hasRoad": false, "owner": ""}, {"row": 10, "col": 4, "hasRoad": false, "owner": ""}, {"row": 0, "col": 0, "hasRoad": false, "owner": ""}, {"row": 7, "col": 1, "hasRoad": false, "owner": ""}, {"row": 4, "col": 5, "hasRoad": false, "owner": ""}, {"row": 8, "col": 2, "hasRoad": false, "owner": ""}, {"row": 9, "col": 3, "hasRoad": false, "owner": ""}, {"row": 6, "col": 0, "hasRoad": false, "owner": ""}, {"row": 2, "col": 3, "hasRoad": false, "owner": ""}, {"row": 0, "col": 5, "hasRoad": false, "owner": ""}, {"row": 8, "col": 7, "hasRoad": false, "owner": ""}, {"row": 10, "col": 1, "hasRoad": false, "owner": ""}, {"row": 4, "col": 2, "hasRoad": false, "owner": ""}, {"row": 1, "col": 0, "hasRoad": false, "owner": ""}, {"row": 6, "col": 5, "hasRoad": false, "owner": ""}, {"row": 5, "col": 3, "hasRoad": false, "owner": ""}, {"row": 0, "col": 1, "hasRoad": false, "owner": ""}, {"row": 2, "col": 7, "hasRoad": false, "owner": ""}, {"row": 10, "col": 5, "hasRoad": false, "owner": ""}, {"row": 8, "col": 3, "hasRoad": false, "owner": ""}, {"row": 7, "col": 0, "hasRoad": false, "owner": ""}, {"row": 4, "col": 6, "hasRoad": false, "owner": ""}, {"row": 6, "col": 8, "hasRoad": false, "owner": ""}, {"row": 9, "col": 2, "hasRoad": false, "owner": ""}, {"row": 6, "col": 1, "hasRoad": false, "owner": ""}, {"row": 3, "col": 1, "hasRoad": false, "owner": ""}, {"row": 7, "col": 4, "hasRoad": false, "owner": ""}, {"row": 2, "col": 0, "hasRoad": false, "owner": ""}, {"row": 4, "col": 3, "hasRoad": false, "owner": ""}, {"row": 5, "col": 2, "hasRoad": false, "owner": ""}, {"row": 0, "col": 2, "hasRoad": false, "owner": ""}, {"row": 8, "col": 4, "hasRoad": false, "owner": ""}], "nodes": [{"row": 4, "col": 7, "building": "", "owner": ""}, {"row": 1, "col": 3, "building": "", "owner": ""}, {"row": 4, "col": 8, "building": "", "owner": ""}, {"row": 3, "col": 0, "building": "", "owner": ""}, {"row": 2, "col": 8, "building": "", "owner": ""}, {"row": 5, "col": 6, "building": "", "owner": ""}, {"row": 2, "col": 1, "building": "", "owner": ""}, {"row": 1, "col": 6, "building": "", "owner": ""}, {"row": 3, "col": 7, "building": "", "owner": ""}, {"row": 0, "col": 3, "building": "", "owner": ""}, {"row": 2, "col": 5, "building": "", "owner": ""}, {"row": 5, "col": 1, "building": "", "owner": ""}, {"row": 2, "col": 4, "building": "", "owner": ""}, {"row": 4, "col": 0, "building": "", "owner": ""}, {"row": 1, "col": 2, "building": "", "owner": ""}, {"row": 3, "col": 3, "building": "", "owner": ""}, {"row": 2, "col": 9, "building": "", "owner": ""}, {"row": 5, "col": 5, "building": "", "owner": ""}, {"row": 3, "col": 10, "building": "", "owner": ""}, {"row": 4, "col": 4, "building": "", "owner": ""}, {"row": 1, "col": 5, "building": "", "owner": ""}, {"row": 3, "col": 6, "building": "", "owner": ""}, {"row": 2, "col": 2, "building": "", "owner": ""}, {"row": 0, "col": 4, "building": "", "owner": ""}, {"row": 5, "col": 0, "building": "", "owner": ""}, {"row": 4, "col": 1, "building": "", "owner": ""}, {"row": 1, "col": 1, "building": "", "owner": ""}, {"row": 3, "col": 2, "building": "", "owner": ""}, {"row": 0, "col": 0, "building": "", "owner": ""}, {"row": 2, "col": 6, "building": "", "owner": ""}, {"row": 5, "col": 4, "building": "", "owner": ""}, {"row": 4, "col": 5, "building": "", "owner": ""}, {"row": 5, "col": 2, "building": "", "owner": ""}, {"row": 1, "col": 4, "building": "", "owner": ""}, {"row": 2, "col": 10, "building": "", "owner": ""}, {"row": 3, "col": 9, "building": "", "owner": ""}, {"row": 0, "col": 5, "building": "", "owner": ""}, {"row": 2, "col": 3, "building": "", "owner": ""}, {"row": 4, "col": 2, "building": "", "owner": ""}, {"row": 1, "col": 0, "building": "", "owner": ""}, {"row": 3, "col": 5, "building": "", "owner": ""}, {"row": 0, "col": 1, "building": "", "owner": ""}, {"row": 2, "col": 7, "building": "", "owner": ""}, {"row": 5, "col": 3, "building": "", "owner": ""}, {"row": 4, "col": 6, "building": "", "owner": ""}, {"row": 3, "col": 1, "building": "", "owner": ""}, {"row": 3, "col": 8, "building": "", "owner": ""}, {"row": 0, "col": 6, "building": "", "owner": ""}, {"row": 1, "col": 8, "building": "", "owner": ""}, {"row": 2, "col": 0, "building": "", "owner": ""}, {"row": 4, "col": 3, "building": "", "owner": ""}, {"row": 1, "col": 7, "building": "", "owner": ""}, {"row": 3, "col": 4, "building": "", "owner": ""}, {"row": 0, "col": 2, "building": "", "owner": ""}]}');
+console.log(testData);
+
+const canWidth = 750;
+const canHeight = 750;
+const numRows = 5;
+const padding = 10;
+const hexLen = 65;
+const setRad = 13;
+
+const hexWidth = hexLen * Math.sqrt(3);
+const horizOffset = hexWidth + padding;
+const vertPad = (Math.sqrt(3) * padding) / 2;
+const vertOffset = (hexLen * 1.5) + vertPad;
+const vco = padding / Math.sqrt(3);
+
+const colorMap = {
+    STONE: "#9c9c9c",
+    SHEEP: "#8eb427",
+    BRICK: "#de5e30",
+    WOOD: "#20953d",
+    WHEAT: "#f2ba38",
+    DESERT: "#c48d52"
+}
+const borderColor = "#c4a060";
+const portColor = "#b97c31";
+
+function draw() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    initBoard(ctx, testData);
+}
+
+function initBoard(ctx, data) {
+    var tilesObj = initHexTiles(ctx, data.tiles);
+    var bgTiles = tilesObj.bgTiles;
+    var hexTiles = tilesObj.hexTiles;
+}
+
+function initHexTiles(tiles) {
+    const out = {
+        hexTiles: [],
+        bgTiles: []
+    };
+
+    const boardWidth = (numRows * hexWidth) + ((numRows + 1) * padding);
+    const boardHeight = ((numRows - 1) * vertOffset) + hexLen * 2;
+    const initX = ((canWidth - boardWidth) / 2) + horizOffset / 2;
+    const initY = (canHeight - boardHeight) / 2;
+
+    for (var i = 0; i < tiles.length; i++) {
+        const curTile = tiles[i];
+        const color = colorMap[curTile.resource];
+        const initXOffset = initX + Math.abs(curTile.row - Math.floor(numRows / 2)) * (horizOffset / 2);
+        const x = initXOffset + horizOffset * curTile.col;
+        const y = initY + curTile.row * vertOffset;
+
+        hexTile = HexTile(curTile.row, curTile.col, x, y, hexLen, color, curTile.rollNum);
+        bgTile = HexTile(curTile.row, curTile.col, x, y - 2 * vco, hexLen + 2 * vco, borderColor, null);
+        out.hexTiles.push(hexTile);
+        out.bgTiles.push(bgTile);
+    }
+
+    return out;
+}
+
+function radians(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+class HexTile {
+    constructor(row, col, x, y, len, color, rollNum) {
+        this.row = row;
+        this.col = col;
+        this.coords = [x, y];
+        for (var i = 1; i < 6; i++) {
+            var nx = coords[i * 2 - 2] + len * Math.cos(radians(60 * (i - 1) + 30));
+            var ny = coords[i * 2 - 1] + len * Math.sin(radians(60 * (i - 1) + 30));
+            coords.push(nx);
+            coords.push(ny);
+        }
+        this.color = color;
+        this.rollNum = rollNum;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(this.coords[0], this.coords[1]);
+        for (var i = 1; i < 6; i++) {
+            ctx.lineTo(this.coords[i * 2], this.coords[i * 2 + 1]);
+        }
+        ctx.fill();
+    }
+}
--- client/client.js
+++ client/client.js
@@ -22,110 +22,110 @@ var others = []
 
 // Connection opened
 socket.addEventListener('open', function (event) {
-  console.log("Connection open")
+    console.log("Connection open")
 });
 
 // Listen for messages
 socket.addEventListener('message', function (event) {
-  var obj = JSON.parse(event.data);
+    var obj = JSON.parse(event.data);
 
-  switch(obj.type) {
-    case CREATED_ROOM:
-      roomId = obj.roomID;
-      playerId = obj.playerID;
-      usercolor = obj.color;
-      addRoomId();
-      appendPlayer(username, usercolor, false);
-      break;
-    case JOINED_ROOM:
-      playerId = obj.playerID;
-      usercolor = obj.color;
-      others = obj.otherPlayers;
-      addRoomId();
-      appendPlayer(username, usercolor, false);
-      addOtherPlayers();
-      break;
-    case PLAYER_JOINED:
-      others.push({name: obj.name, color: obj.color, isReady: false});
-      appendPlayer(obj.name, obj.color, false);
-      break;
-    case READY_SUCCESS:
-      // do nothing
-      break;
-    case PLAYER_READY:
-      displayReady(obj.name);
-      break;
-    default:
-      // code block
-  };
+    switch(obj.type) {
+        case CREATED_ROOM:
+        roomId = obj.roomID;
+        playerId = obj.playerID;
+        usercolor = obj.color;
+        addRoomId();
+        appendPlayer(username, usercolor, false);
+        break;
+        case JOINED_ROOM:
+        playerId = obj.playerID;
+        usercolor = obj.color;
+        others = obj.otherPlayers;
+        addRoomId();
+        appendPlayer(username, usercolor, false);
+        addOtherPlayers();
+        break;
+        case PLAYER_JOINED:
+        others.push({name: obj.name, color: obj.color, isReady: false});
+        appendPlayer(obj.name, obj.color, false);
+        break;
+        case READY_SUCCESS:
+        // do nothing
+        break;
+        case PLAYER_READY:
+        displayReady(obj.name);
+        break;
+        default:
+        // code block
+    };
 
-  console.log(obj);
+    console.log(obj);
 });
 
 function addRoomId() {
-  document.getElementById("roomHeader").innerHTML = `Room ID: ${roomId}`;
+    document.getElementById("roomHeader").innerHTML = `Room ID: ${roomId}`;
 }
 
 function addOtherPlayers() {
-  var playerList = document.getElementById('playerList');
-  console.log(others)
-  for (var i = 0; i < others.length; i++) {
-    const plyr = others[i];
-    console.log(plyr)
-    appendPlayer(plyr.name, plyr.color, plyr.isReady);
-  }
+    var playerList = document.getElementById('playerList');
+    console.log(others)
+    for (var i = 0; i < others.length; i++) {
+        const plyr = others[i];
+        console.log(plyr)
+        appendPlayer(plyr.name, plyr.color, plyr.isReady);
+    }
 }
 
 function appendPlayer(playerName, playerColor, playerReady) {
-  var playerList = document.getElementById('playerList');
-  var newPlayer = document.createElement("li");
+    var playerList = document.getElementById('playerList');
+    var newPlayer = document.createElement("li");
 
-  var nameText = document.createTextNode("Name: ");
-  var colorText = document.createTextNode(", Color: ");
-  var readyText = document.createTextNode(", Ready: ");
+    var nameText = document.createTextNode("Name: ");
+    var colorText = document.createTextNode(", Color: ");
+    var readyText = document.createTextNode(", Ready: ");
 
-  var nameSpan = document.createElement("span");
-  nameSpan.id = `${playerName}_NameSpan`;
-  nameSpan.innerHTML = `${playerName}`;
+    var nameSpan = document.createElement("span");
+    nameSpan.id = `${playerName}_NameSpan`;
+    nameSpan.innerHTML = `${playerName}`;
 
-  var colorSpan = document.createElement("span");
-  colorSpan.id = `${playerName}_ColorSpan`;
-  colorSpan.innerHTML = `${playerColor}`;
+    var colorSpan = document.createElement("span");
+    colorSpan.id = `${playerName}_ColorSpan`;
+    colorSpan.innerHTML = `${playerColor}`;
 
-  var readySpan = document.createElement("span");
-  readySpan.id = `${playerName}_ReadySpan`;
-  readySpan.innerHTML = `${playerReady}`;
+    var readySpan = document.createElement("span");
+    readySpan.id = `${playerName}_ReadySpan`;
+    readySpan.innerHTML = `${playerReady}`;
 
-  newPlayer.appendChild(nameText);
-  newPlayer.appendChild(nameSpan);
-  newPlayer.appendChild(colorText);
-  newPlayer.appendChild(colorSpan);
-  newPlayer.appendChild(readyText);
-  newPlayer.appendChild(readySpan);
+    newPlayer.appendChild(nameText);
+    newPlayer.appendChild(nameSpan);
+    newPlayer.appendChild(colorText);
+    newPlayer.appendChild(colorSpan);
+    newPlayer.appendChild(readyText);
+    newPlayer.appendChild(readySpan);
 
-  playerList.appendChild(newPlayer);
+    playerList.appendChild(newPlayer);
 }
 
 function displayReady(playerName) {
-  var readySpan = document.getElementById(`${playerName}_ReadySpan`);
-  readySpan.innerHTML = `${true}`;
+    var readySpan = document.getElementById(`${playerName}_ReadySpan`);
+    readySpan.innerHTML = `${true}`;
 }
 
 function createRoom() {
-  username = document.getElementById('nameInput').value;
-  var out = {type: CREATE_ROOM, name: username};
-  socket.send(JSON.stringify(out));
+    username = document.getElementById('nameInput').value;
+    var out = {type: CREATE_ROOM, name: username};
+    socket.send(JSON.stringify(out));
 }
 
 function joinRoom() {
-  username = document.getElementById('nameInput').value;
-  roomId = parseInt(document.getElementById('roomIdInput').value, 10);
-  var out = {type: JOIN_ROOM, name: username, roomID: roomId};
-  socket.send(JSON.stringify(out));
+    username = document.getElementById('nameInput').value;
+    roomId = parseInt(document.getElementById('roomIdInput').value, 10);
+    var out = {type: JOIN_ROOM, name: username, roomID: roomId};
+    socket.send(JSON.stringify(out));
 }
 
 function markReady() {
-  displayReady(username);
-  var out = {type: READY, playerID: playerId, roomID: roomId};
-  socket.send(JSON.stringify(out));
+    displayReady(username);
+    var out = {type: READY, playerID: playerId, roomID: roomId};
+    socket.send(JSON.stringify(out));
 }
