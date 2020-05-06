fc8e15d32c4d14fe5dbdd19279a4e4e0696449ec
Liam Douglass <liamadouglass@gmail.com>
2020-05-06 12:53:38 -0400

    finished adding types to client code

--- client/client.ts
+++ client/client.ts
@@ -1,43 +1,89 @@
-// Outgoing types
-const CREATE_ROOM = "CREATE_ROOM"
-const JOIN_ROOM = "JOIN_ROOM"
-const READY = "READY"
+interface ModelPlayer {
+    name: string;
+    color: string;
+    isReady: boolean;
+}
 
-// Incoming types
-const CREATED_ROOM = "CREATED_ROOM"
-const JOINED_ROOM = "JOINED_ROOM"
-const PLAYER_JOINED = "PLAYER_JOINED"
-const READY_SUCCESS = "READY_SUCCESS"
-const PLAYER_READY = "PLAYER_READY"
-const ERROR = "ERROR"
+interface CreatedRoom {
+    type: "CREATED_ROOM";
+    roomID: number;
+    playerID: string;
+    color: string;
+}
 
+interface JoinedRoom {
+    type: "JOINED_ROOM";
+    playerID: string;
+    color: string;
+    otherPlayers: ModelPlayer[];
+}
+
+interface PlayerJoined {
+    type: "PLAYER_JOINED";
+    name: string;
+    color: string;
+}
+
+interface ReadySuccess {
+    type: "READY_SUCCESS";
+}
+
+interface PlayerReady {
+    type: "PLAYER_READY";
+    name: string;
+}
+
+interface Error {
+    type: "ERROR";
+}
+
+type InputMessage = CreatedRoom | JoinedRoom | PlayerJoined | ReadySuccess | PlayerReady | Error;
+
+interface CreateRoom {
+    type: "CREATE_ROOM";
+    name: string;
+}
+
+interface JoinRoom {
+    type: "JOIN_ROOM";
+    name: string;
+    roomID: number;
+}
+
+interface Ready {
+    type: "READY";
+    playerID: string;
+    roomID: number;
+}
+
+type OutputMessage = CreateRoom | JoinRoom | Ready;
 
 // Create WebSocket connection.
-const socket = new WebSocket('ws://localhost:8765');
-var playerId;
-var roomId;
-var username;
-var usercolor;
-var others = []
+const socket: WebSocket = new WebSocket('ws://localhost:8765');
+var playerId: string;
+var roomId: number;
+var username: string;
+var usercolor: string;
+var others: ModelPlayer[] = [];
 
 // Connection opened
-socket.addEventListener('open', function (event) {
-    console.log("Connection open")
+socket.addEventListener('open', function (event: Event): void {
+    console.log("Connection open");
 });
 
 // Listen for messages
-socket.addEventListener('message', function (event) {
-    var obj = JSON.parse(event.data);
+socket.addEventListener('message', function (event: MessageEvent) {
+    var obj: InputMessage = JSON.parse(event.data);
 
     switch(obj.type) {
-        case CREATED_ROOM:
+        case "CREATED_ROOM":
             roomId = obj.roomID;
             playerId = obj.playerID;
             usercolor = obj.color;
             addRoomId();
             appendPlayer(username, usercolor, false);
             break;
-        case JOINED_ROOM:
+        case "JOINED_ROOM":
             playerId = obj.playerID;
             usercolor = obj.color;
             others = obj.otherPlayers;
@@ -45,55 +91,51 @@ socket.addEventListener('message', function (event) {
             appendPlayer(username, usercolor, false);
             addOtherPlayers();
             break;
-        case PLAYER_JOINED:
+        case "PLAYER_JOINED":
             others.push({name: obj.name, color: obj.color, isReady: false});
             appendPlayer(obj.name, obj.color, false);
             break;
-        case READY_SUCCESS:
+        case "READY_SUCCESS":
             // do nothing
             break;
-        case PLAYER_READY:
+        case "PLAYER_READY":
             displayReady(obj.name);
             break;
             default:
             // code block
     }
-
-    console.log(obj);
 });
 
-function addRoomId() {
+function addRoomId(): void {
     document.getElementById("roomHeader").innerHTML = `Room ID: ${roomId}`;
 }
 
-function addOtherPlayers() {
-    console.log(others)
+function addOtherPlayers(): void {
     for (var i = 0; i < others.length; i++) {
         const plyr = others[i];
-        console.log(plyr)
         appendPlayer(plyr.name, plyr.color, plyr.isReady);
     }
 }
 
-function appendPlayer(playerName, playerColor, playerReady) {
-    var playerList = document.getElementById('playerList');
-    var newPlayer = document.createElement("li");
+function appendPlayer(playerName: string, playerColor: string, playerReady: boolean): void {
+    var playerList: HTMLElement = document.getElementById('playerList');
+    var newPlayer: HTMLElement = document.createElement("li");
 
-    var nameText = document.createTextNode("Name: ");
-    var colorText = document.createTextNode(", Color: ");
-    var readyText = document.createTextNode(", Ready: ");
+    var nameText: Text = document.createTextNode("Name: ");
+    var colorText: Text = document.createTextNode(", Color: ");
+    var readyText: Text = document.createTextNode(", Ready: ");
 
-    var nameSpan = document.createElement("span");
+    var nameSpan: HTMLElement = document.createElement("span");
     nameSpan.id = `${playerName}_NameSpan`;
-    nameSpan.innerHTML = `${playerName}`;
+    nameSpan.innerHTML = playerName;
 
-    var colorSpan = document.createElement("span");
+    var colorSpan: HTMLElement = document.createElement("span");
     colorSpan.id = `${playerName}_ColorSpan`;
-    colorSpan.innerHTML = `${playerColor}`;
+    colorSpan.innerHTML = playerColor;
 
-    var readySpan = document.createElement("span");
+    var readySpan: HTMLElement = document.createElement("span");
     readySpan.id = `${playerName}_ReadySpan`;
-    readySpan.innerHTML = `${playerReady}`;
+    readySpan.innerHTML = playerName;
 
     newPlayer.appendChild(nameText);
     newPlayer.appendChild(nameSpan);
@@ -105,26 +147,30 @@ function appendPlayer(playerName, playerColor, playerReady) {
     playerList.appendChild(newPlayer);
 }
 
-function displayReady(playerName) {
-    var readySpan = document.getElementById(`${playerName}_ReadySpan`);
-    readySpan.innerHTML = `${true}`;
+function displayReady(playerName: string): void {
+    var readySpan: HTMLElement = document.getElementById(`${playerName}_ReadySpan`);
+    readySpan.innerHTML = true.toString();
 }
 
-function createRoom() {
-    username = document.getElementById('nameInput').value;
-    var out = {type: CREATE_ROOM, name: username};
-    socket.send(JSON.stringify(out));
+function createRoom(): void {
+    username = (document.getElementById('nameInput') as HTMLInputElement).value;
+    var out: CreateRoom = {type: "CREATE_ROOM", name: username};
+    sendMessage(out);
 }
 
-function joinRoom() {
-    username = document.getElementById('nameInput').value;
-    roomId = parseInt(document.getElementById('roomIdInput').value, 10);
-    var out = {type: JOIN_ROOM, name: username, roomID: roomId};
-    socket.send(JSON.stringify(out));
+function joinRoom(): void {
+    username = (document.getElementById('nameInput') as HTMLInputElement).value;
+    roomId = parseInt((document.getElementById('roomIdInput') as HTMLInputElement).value, 10);
+    var out: JoinRoom = {type: "JOIN_ROOM", name: username, roomID: roomId};
+    sendMessage(out);
 }
 
-function markReady() {
+function markReady(): void {
     displayReady(username);
-    var out = {type: READY, playerID: playerId, roomID: roomId};
-    socket.send(JSON.stringify(out));
+    var out: Ready = {type: "READY", playerID: playerId, roomID: roomId};
+    sendMessage(out);
+}
+
+function sendMessage(msg: OutputMessage): void {
+    socket.send(JSON.stringify(msg));
 }
