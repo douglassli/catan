a9a2531368613e4a532b21a7d0500050574ec269
Liam Douglass <liamadouglass@gmail.com>
2020-05-16 09:24:53 -0400

    updated and improved selenium automated test script

--- selenium_test.py
+++ selenium_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver as wd
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 
@@ -9,12 +11,12 @@ def put_name(driver, name):
 
 def click_create(driver):
     create_button = driver.find_element_by_id("createRoomButton")
-    create_button.click()
+    create_button.send_keys(Keys.ENTER)
 
 
 def click_ready(driver):
     mark_ready_button = driver.find_element_by_id("readyButton")
-    mark_ready_button.click()
+    mark_ready_button.send_keys(Keys.ENTER)
 
 
 def get_room_id(driver):
@@ -29,12 +31,12 @@ def put_room_id(driver, room_id):
 
 def click_join(driver):
     join_button = driver.find_element_by_id("joinRoomButton")
-    join_button.click()
+    join_button.send_keys(Keys.ENTER)
 
 
 def click_start(driver):
     start_button = driver.find_element_by_id("startButton")
-    start_button.click()
+    start_button.send_keys(Keys.ENTER)
 
 
 def test1():
@@ -44,20 +46,23 @@ def test1():
     d.execute_script("window.open('http://127.0.0.1:8080/room')")
     d.execute_script("window.open('http://127.0.0.1:8080/room')")
 
-    time.sleep(1)
-
     sizes = [(0, 23, 961, 527), (959, 23, 961, 527), (0, 550, 961, 527), (959, 550, 961, 527)]
 
-    d.set_window_rect(*sizes[0])
+    for i in range(4):
+        d.switch_to.window(d.window_handles[i])
+        time.sleep(0.25)
+        d.set_window_rect(*sizes[i])
+
     d.switch_to.window(d.window_handles[0])
+    time.sleep(0.25)
     put_name(d, "p1")
     click_create(d)
     rid = get_room_id(d)
     click_ready(d)
 
     for i in range(1, 4):
-        d.set_window_rect(*sizes[i])
         d.switch_to.window(d.window_handles[i])
+        time.sleep(0.25)
         put_name(d, "p{}".format(i + 1))
         put_room_id(d, rid)
         click_join(d)
