c1c1de31e2f762b42f6417ece4b86658cec4d186
Liam Douglass <liamadouglass@gmail.com>
2020-05-17 18:42:17 -0400

    removed old python tkinter based view in favor of the new html/svg frontend

--- view/node_pos.py
+++
@@ -1,7 +0,0 @@
-class Node:
-    def __init__(self, row, col, x, y, building):
-        self.row = row
-        self.col = col
-        self.x = x
-        self.y = y
-        self.building = building
