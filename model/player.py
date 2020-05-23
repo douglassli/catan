b5ca87d774924dd7185719b44fc3975de9af4dd2
Liam Douglass <liamadouglass@gmail.com>
2020-05-23 11:14:56 -0400

    refactored resource checking into seperate function in preperation for implementation of trading

--- model/player.py
+++ model/player.py
@@ -20,23 +20,21 @@ class Player:
 
     def can_build_settle(self):
         has_settles = self.num_settles >= 1
-        has_resources = self.resources[Resource.WOOD] >= 1 and self.resources[Resource.WHEAT] >= 1 and \
-                        self.resources[Resource.BRICK] >= 1 and self.resources[Resource.SHEEP] >= 1
+        has_resources = self.has_resources({Resource.WOOD: 1, Resource.WHEAT: 1, Resource.BRICK: 1, Resource.SHEEP: 1})
         return has_settles and has_resources
 
     def can_build_road(self):
         has_roads = self.num_roads >= 1
-        has_resources = self.resources[Resource.WOOD] >= 1 and self.resources[Resource.BRICK] >= 1
+        has_resources = self.has_resources({Resource.WOOD: 1, Resource.BRICK: 1})
         return has_roads and has_resources
 
     def can_build_city(self):
         has_cities = self.num_cities >= 1
-        has_resources = self.resources[Resource.WHEAT] >= 2 and self.resources[Resource.STONE] >= 3
+        has_resources = self.has_resources({Resource.WHEAT: 2, Resource.STONE: 3})
         return has_cities and has_resources
 
     def can_buy_dev_card(self):
-        has_resources = self.resources[Resource.WHEAT] >= 1 and self.resources[Resource.STONE] >= 1 and \
-                        self.resources[Resource.SHEEP] >= 1
+        has_resources = self.has_resources({Resource.WHEAT: 1, Resource.STONE: 1, Resource.SHEEP: 1})
         return has_resources
 
     def buy_road(self, is_setup):
@@ -94,3 +92,9 @@ class Player:
         resource = choices([res for res in self.resources], weights=weights, k=1)[0]
         self.resources[resource] -= 1
         return resource
+
+    def has_resources(self, resources):
+        for res, val in resources.items():
+            if self.resources[res] < val:
+                return False
+        return True
